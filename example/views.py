671da02f87b2d52989e93f1bad9d9c1e971cf47e
AmehdFM <94817400+AmehdFM@users.noreply.github.com>
2024-02-17 14:20:22 -0600

    a ver si funciona en vercel asi

--- example/views.py
+++ example/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-from supabase import create_client, Client
 from django.http import JsonResponse
 
 def index(request):
